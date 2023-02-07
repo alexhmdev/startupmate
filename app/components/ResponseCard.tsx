2cb16304663c383e9d8e5f0964f1607a51e8edba
AlexHM <alex13pks@gmail.com>
2023-02-07 08:28:38 -0600

    fix(responsecard): response card size

--- app/components/ResponseCard.tsx
+++ app/components/ResponseCard.tsx
@@ -6,7 +6,7 @@ export default function ResponseCard({ cardBody }: { cardBody: string }) {
       <Clipboard content={cardBody} className="absolute top-1 right-1" />
       <div className="card-body ">
         <textarea
-          className="min-h-[200px] max-h[400px] flex-grow resize-none  w-full rounded-md border border-gray-90 bg-transparent py-3 px-4   read-only:border-none read-only:bg-primary-light disabled:border-gray-90 disabled:bg-gray-95 outline-primary-neutral focus-visible:outline focus-visible:outline-1 focus-visible:outline-none"
+          className="min-h-[150px] max-h[400px] flex-grow resize-none  w-full rounded-md border border-gray-90 bg-transparent py-3 px-4   read-only:border-none read-only:bg-primary-light disabled:border-gray-90 disabled:bg-gray-95 outline-primary-neutral focus-visible:outline focus-visible:outline-1 focus-visible:outline-none"
           readOnly
           spellCheck="false"
           value={cardBody}
